e62c542c8c30a9c081b36fd63aad6a5b4f5ae7f9
Dmytro Salenko <salenko.d@pm.me>
2020-01-15 15:17:13 +0200

    fixed date filter navigation state

--- app/containers/RewardsFilters/FilterByDate.js
+++ app/containers/RewardsFilters/FilterByDate.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import DatePicker from 'react-datepicker';
 import { DateTime } from 'luxon';
 import styled from 'styled-components';
@@ -30,23 +30,17 @@ const StyledDatePicker = styled(DatePicker)`
 
 const FilterByDate = () => {
   const [queryParams, setQueryParam] = useQueryParams();
-  const [startDate, setStartDate] = useState(getDateValue(queryParams.from));
-  const [endDate, setEndDate] = useState(getDateValue(queryParams.to));
-
-  useEffect(() => {
-    setQueryParam('from', getDateParam(startDate));
-  }, [startDate]);
-
-  useEffect(() => {
-    setQueryParam('to', getDateParam(endDate));
-  }, [endDate]);
+  const startDate = useMemo(() => getDateValue(queryParams.from), [
+    queryParams.from,
+  ]);
+  const endDate = useMemo(() => getDateValue(queryParams.to), [queryParams.to]);
 
   const handleChangeStartDate = value => {
-    setStartDate(value);
+    setQueryParam('from', getDateParam(value));
   };
 
   const handleChangeEndDate = value => {
-    setEndDate(value);
+    setQueryParam('to', getDateParam(value));
   };
 
   return (
